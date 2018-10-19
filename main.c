29089ab424e46ed13f019a2d1309198cf734ab27
angelolvera00 <A01411230@itesm.mx>
2018-10-18 19:31:03 -0500

    commit

---
+++ main.c
@@ -0,0 +1,26 @@
+/*This program was created for the purpose of asking the user to enter a string and the program will check its length.
+ *
+ * Author: Angel Daniel Olvera Perales
+ * Date: October 17
+ * email:angelolvera00@gmail.com
+ */
+#include <stdio.h>
+int main( ) {
+/*Declares the variables that I'll be using, one is for stating the maximum length that a string can have
+and the other one is for stating the length of the string.
+ */
+    char string[10000];
+    int length=0;
+//Asks the user for a string and scans it.
+    printf("Enter a string: \n");
+    gets(string);
+//The while loop will keep on scanning every character entered in the string until it runs out of information,
+// it will keep adding every number to the length.
+   while(string[length]!='\0')
+    {
+        length++;
+    }
+// The length of the string is printed.
+    printf("Length of string is %d", length);
+    return 0;
+}
